cae2e329c6eea3e79065eed28ee6ad277a857e07
Tony Sarni <sarni.tony@gmail.com>
2020-01-10 00:25:23 -0600

    fixed some errors in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,17 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const db = require('./models/');
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(logger('dev'));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true , useUnifiedTopology: true });
-
-app.use(require('./routes/api-routes.js'));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
